e4ac726775f37f170379119aafd4471149e837f4
prasana rath <prasanpkr143@gmail.com>
2025-02-08 18:14:33 +0530

    Deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const session = require('express-session');
 
 const app = express();
-const port = 3000;
+const port =process.env.PORT  || 3000;
 
 // Middleware
 app.use(express.static(__dirname));
